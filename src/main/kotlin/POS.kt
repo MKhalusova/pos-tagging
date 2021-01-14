832d9d70d246ebcba2dba70492d883e7e1dab7ce
MKhalusova <kafooster@gmail.com>
2021-01-14 09:39:19 -0500

    minor improvements

--- src/main/kotlin/POS.kt
+++ src/main/kotlin/POS.kt
@@ -4,19 +4,20 @@ fun main() {
     val trainingCorpus = File("src/data/WSJ_02-21.pos").readLines()
 
     val vocab = File("src/data/vocab.txt").readLines()
-    val vocabMap =
-        vocab.mapIndexed { index: Int, s: String -> index to s }.toMap().entries.associate { (k, v) -> v to k }
+    val vocabMap = vocab
+        .withIndex()
+        .associate { (index, s) -> s to index }
 
-    /* preparing the test data */
+//  preparing the test data
     val preprocessor = Preprocessor
     val testWordsAndTags = preprocessor.getTestWordsAndTags(vocabMap, File("src/data/WSJ_24.pos"))
     val testWords = testWordsAndTags.map { it.first }
     val testTags = testWordsAndTags.map { it.second }
 
-    /* "training" the Hidden Markov Model on the training data */
+//  "training" the Hidden Markov Model on the training data
     val hmm = HMM(trainingCorpus, vocabMap)
 
-    /* evaluating the HMM model on the test part of the corpus */
+//  evaluating the HMM model on the test part of the corpus
     println(hmm.score(testWords, testTags))
 
 }
--- src/main/kotlin/Preprocessor.kt
+++ src/main/kotlin/Preprocessor.kt
@@ -2,7 +2,7 @@ import java.io.File
 
 object Preprocessor {
 
-    /* heuristics for replacing words that are not in the vocabulary with unk-[...] */
+//  heuristics for replacing words that are not in the vocabulary with unk-[...]
     private val nounSuffix = listOf(
         "action", "age", "ance", "cy", "dom", "ee", "ence", "er", "hood",
         "ion", "ism", "ist", "ity", "ling", "ment", "ness", "or", "ry", "scape", "ship", "ty"
@@ -15,30 +15,26 @@ object Preprocessor {
     private val advSuffix = listOf("ward", "wards", "wise")
 
 
-    fun assignUnk(word: String): String {
-        when {
-            word.contains(Regex("[0-9]")) -> return "--unk_digit--"
-            word.contains(Regex("[^A-Za-z0-9 ]")) -> return "--unk_punct--"
-            word.contains(Regex("[A-Z]")) -> return "--unk_upper--"
-            nounSuffix.any { word.endsWith(it) } -> return "--unk_noun--"
-            verbSuffix.any { word.endsWith(it) } -> return "--unk_verb--"
-            adjSuffix.any { word.endsWith(it) } -> return "--unk_adj--"
-            advSuffix.any { word.endsWith(it) } -> return "--unk_adv--"
-            else -> return "--unk--"
-        }
+    fun assignUnk(word: String): String = when {
+        word.contains(Regex("[0-9]")) -> "--unk_digit--"
+        word.contains(Regex("[^A-Za-z0-9 ]")) -> "--unk_punct--"
+        word.contains(Regex("[A-Z]")) -> "--unk_upper--"
+        nounSuffix.any { word.endsWith(it) } -> "--unk_noun--"
+        verbSuffix.any { word.endsWith(it) } -> "--unk_verb--"
+        adjSuffix.any { word.endsWith(it) } -> "--unk_adj--"
+        advSuffix.any { word.endsWith(it) } -> "--unk_adv--"
+        else -> "--unk--"
     }
 
 
-    fun getWordAndTagFromLine(line: String, vocab: Map<String, Int>): Pair<String, String> {
+    fun getWordAndTagFromLine(line: String, vocab: Map<String, Int>): Pair<String, String> =
         if (line.split('\t').isNotEmpty() && line.split('\t').size == 2) {
             var word = line.split('\t')[0]
             val tag = line.split('\t')[1]
-            if (word !in vocab.keys) {
-                word = this.assignUnk(word)
-            }
-            return Pair(word, tag)
-        } else return Pair("--n--", "--s--")
-    }
+            if (!vocab.containsKey(word)) word = assignUnk(word)
+            Pair(word, tag)
+        } else Pair("--n--", "--s--")
+
 
     fun getTestWordsAndTags(vocab: Map<String, Int>, testDataFile: File): List<Pair<String, String>> {
         val wordsAndTags = mutableListOf<Pair<String, String>>()
